af84c55bd5bee4e46a699e24ae77adb5ca7083e2
Abdullah <xdaxer@outlook.com.tr>
2024-01-23 13:33:08 +0300

    Note App

---
+++ demos/note-app/main.js
@@ -0,0 +1,23 @@
+let input = document.querySelector("input");
+let addBtn = document.querySelector("#addBtn");
+let clearBtn = document.querySelector("#clearBtn");
+let noteList = document.querySelector("ul");
+let notes = [];
+
+addBtn.addEventListener("click", function () {
+  if (input.value > 0) {
+    notes.push(input.value);
+    console.log(notes);
+
+    let newNote = document.createElement("li");
+    newNote.textContent = notes[notes.length - 1];
+    noteList.append(newNote);
+  } else {
+    alert("error");
+  }
+  input.value = "";
+});
+
+clearBtn.addEventListener("click", function () {
+  noteList.innerHTML = "";
+});
