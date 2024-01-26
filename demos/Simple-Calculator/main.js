2a469197ddd0d12445ad05ecbcd330bc27c876c6
Abdullah <xdaxer@outlook.com.tr>
2024-01-26 11:39:50 +0300

    Simple-Calculator

--- demos/Simple-Calculator/main.js
+++ demos/Simple-Calculator/main.js
@@ -1,55 +1,110 @@
 let firstCount = document.querySelector("#firstCount");
 let secondCount = document.querySelector("#secondCount");
-let add = document.querySelector("#add");
-let subtract = document.querySelector("#subtract");
-let divide = document.querySelector("#divide");
-let multiply = document.querySelector("#multiply");
+let additionButton = document.querySelector("#addition");
+let subtractionButton = document.querySelector("#subtraction");
+let multiplicationButton = document.querySelector("#multiplication");
+let divisionButton = document.querySelector("#division");
 let result = document.querySelector("#result");
+let clearButton = document.querySelector("#clear");
 
-add.addEventListener("click", function () {
-  if (firstCount.value.length == "" || secondCount.value.length == "") {
-    alert("Error");
+// Addition
+
+function addition(newFirstCount, newSecondCount) {
+  return newFirstCount + newSecondCount;
+}
+
+additionButton.addEventListener("click", function () {
+  if (firstCount.value !== "" && secondCount.value !== "") {
+    let firstValue = parseFloat(firstCount.value);
+    let secondValue = parseFloat(secondCount.value);
+    result.textContent = `${firstCount.value} + ${
+      secondCount.value
+    } = ${addition(firstValue, secondValue)}`;
   } else {
-    result.innerHTML = `
-  ${firstCount.value} + ${secondCount.value} = ${
-      parseFloat(firstCount.value) + parseFloat(secondCount.value)
-    }
-    
-  `;
+    alert("Please Enter a Value");
+    result.textContent = "  ";
   }
 
   firstCount.value = "";
   secondCount.value = "";
 });
 
-subtract.addEventListener("click", function () {
-  if (firstCount.value.length == "" || secondCount.value.length == "") {
-    alert("Error");
+//  Subtraction
+
+function subtraction(newFirstCount, newSecondCount) {
+  return newFirstCount - newSecondCount;
+}
+
+subtractionButton.addEventListener("click", function () {
+  if (firstCount.value !== "" && secondCount.value !== "") {
+    let firstValue = parseFloat(firstCount.value);
+    let secondValue = parseFloat(secondCount.value);
+    result.textContent = `${firstCount.value} - ${
+      secondCount.value
+    } = ${subtraction(firstValue, secondValue)}`;
   } else {
-    result.innerHTML = `
-  ${firstCount.value} - ${secondCount.value} = ${
-      parseFloat(firstCount.value) - parseFloat(secondCount.value)
-    }
-    
-  `;
+    alert("Please Enter a Value");
+    result.textContent = "  ";
   }
 
   firstCount.value = "";
   secondCount.value = "";
 });
 
-multiply.addEventListener("click", function () {
-  if (firstCount.value.length == "" || secondCount.value.length == "") {
-    alert("Error");
+// Multiplication;
+
+function multiplication(newFirstCount, newSecondCount) {
+  return newFirstCount * newSecondCount;
+}
+
+multiplicationButton.addEventListener("click", function () {
+  if (firstCount.value !== "" && secondCount.value !== "") {
+    let firstValue = parseFloat(firstCount.value);
+    let secondValue = parseFloat(secondCount.value);
+    result.textContent = `${firstCount.value} * ${
+      secondCount.value
+    } = ${multiplication(firstValue, secondValue)}`;
+
+    firstCount.value = "";
+    secondCount.value = "";
   } else {
-    result.innerHTML = `
-  ${firstCount.value} x ${secondCount.value} = ${
-      parseFloat(firstCount.value) * parseFloat(secondCount.value)
-    }
-    
-  `;
+    alert("Please Enter a Value");
+    result.textContent = "  ";
   }
 
   firstCount.value = "";
   secondCount.value = "";
+});
+
+// Division
+
+function division(newFirstCount, newSecondCount) {
+  return newFirstCount / newSecondCount;
+}
+
+divisionButton.addEventListener("click", function () {
+  if (firstCount.value !== "" && secondCount.value !== "") {
+    let firstValue = parseFloat(firstCount.value);
+    let secondValue = parseFloat(secondCount.value);
+    result.textContent = `${firstCount.value} / ${
+      secondCount.value
+    } = ${division(firstValue, secondValue)}`;
+  } else {
+    alert("Please Enter a Value");
+    result.textContent = "  ";
+  }
+
+  if (firstCount.value == 0 || secondCount.value == 0) {
+    alert("You cannot use 0 in division");
+    result.textContent = "  ";
+  }
+
+  firstCount.value = "";
+  secondCount.value = "";
+});
+
+clearButton.addEventListener("click", function () {
+  firstCount.value = "";
+  secondCount.value = "";
+  result.textContent = "";
 });
